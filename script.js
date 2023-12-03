43a4f5610eee48142caafd321c56e38d0660d1b0
NathanD <nathand50444@gmail.com>
2023-12-03 20:10:28 +0000

    Option allows users to set parameters (rows/cols).
    An option hs now been added to allow th4e user to define the rows and columns of the etch-a-sketch.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const container = document.getElementById("gridContainer");
+    parameterButton = document.getElementById("parameters");
+
     function generateGrid(rows, columns) {
         container.innerHTML = ''; 
-
-    const container = document.getElementById("gridContainer");
-    parameterButton = document.getElementById("parameters")
 
     for (let i = 1; i <= rows * columns; i++) {
         const gridItem = document.createElement("div");
@@ -18,11 +18,29 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
     
-    parameterButton.addEventListener("click", function() {
-        const rowsInput = promt("Enter the number of rows:");
+parameterButton.addEventListener("click", function() {
+    const rowsInput = prompt("Enter the number of rows:");
 
-        if (rowsInput !== null) {
-            
+    if (rowsInput !== null) {
+        const rows = parseInt(rowsInput.trim(), 10);
+      
+        if (!isNaN(rows) && rows > 0 && rows <= 100) {
+            const columnsInput = prompt("Enter the number of columns:");
+              
+            if (columnsInput !== null) {
+            const columns = parseInt(columnsInput.trim(), 10);
+      
+            if (!isNaN(columns) && columns > 0 && columns <= 100) {
+                  generateGrid(rows, columns);
+            } else {
+                  alert("Please enter a valid number of columns.");
+            }
+            }
+        } else {
+            alert("Please enter a valid number of rows.");
         }
-      });
+        }
     });
+        
+    generateGrid(16, 16);
+});
